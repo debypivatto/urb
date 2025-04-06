90703be961da939dd79321675145aef7092b8b83
Debora Pivatto <deborapivatto@gmail.com>
2025-04-06 15:46:41 +0100

    projectnew

--- project5.py
+++ project5.py
@@ -3,23 +3,28 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-df_car_data = pd.read_csv('vehicles.csv')
+df_car_data = pd.read_csv('vehicles.csv') # carregar os dados do arquivo
 
-fig = px.histogram(df_car_data, x="odometer") # criar um histograma
+st.title ('Análise de Dados de Veículos')
+
+fig = px.histogram(df_car_data, x="odometer") # criar um histograma dos carros
 fig.show() # exibindo
 
-fig = px.scatter(df_car_data, x="odometer", y="price") # criar um gráfico de dispersão
+fig = px.scatter(df_car_data, x="odometer", y="price") # criar um gráfico de dispersão por preco
 fig.show() # exibindo
 
-hist_button = st.button('Criar histograma') # criar um botão
-if hist_button: # se o botão for clicado
-         # escrever uma mensagem
-        st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
-            
-# criar um histograma
-fig = px.histogram(df_car_data, x="odometer")  # exibir um gráfico Plotly interativo
-st.plotly_chart(fig, use_container_width=True)
-# criar uma caixa de seleção
-build_histogram = st.checkbox('Criar um histograma')
-if build_histogram: # se a caixa de seleção for selecionada
+if st.button('Criar histograma'):
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.histogram(df_car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Caixa de seleção para criar histograma
+if st.checkbox('Criar outro histograma (por odômetro)'):
     st.write('Criando um histograma para a coluna odometer')
+    fig = px.histogram(df_car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Gráfico de dispersão sempre exibido
+st.write("Gráfico de dispersão entre odômetro e preço")
+fig = px.scatter(df_car_data, x="odometer", y="price")
+st.plotly_chart(fig, use_container_width=True)
